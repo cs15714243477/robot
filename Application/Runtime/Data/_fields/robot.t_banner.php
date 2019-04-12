17eff17c5b74af943fad39de8cf31dc0aa79670b
cui <1729917782@qq.com>
2019-04-12 10:35:21 +0800

    Signed-off-by: cui <1729917782@qq.com>

--- Application/Runtime/Data/_fields/robot.t_banner.php
+++
@@ -1,1 +0,0 @@
-a:8:{i:0;s:2:"id";i:1;s:6:"images";i:2;s:7:"addtime";i:3;s:4:"sort";i:4;s:6:"switch";i:5;s:9:"oneswitch";s:3:"_pk";s:2:"id";s:5:"_type";a:6:{s:2:"id";s:7:"int(11)";s:6:"images";s:12:"varchar(255)";s:7:"addtime";s:8:"datetime";s:4:"sort";s:7:"int(11)";s:6:"switch";s:10:"tinyint(1)";s:9:"oneswitch";s:10:"tinyint(1)";}}
--- Application/Runtime/Data/_fields/robot.t_video_cn.php
+++
@@ -1,1 +0,0 @@
-a:9:{i:0;s:2:"id";i:1;s:3:"pid";i:2;s:5:"title";i:3;s:8:"onetitle";i:4;s:5:"image";i:5;s:4:"file";i:6;s:6:"switch";s:3:"_pk";s:2:"id";s:5:"_type";a:7:{s:2:"id";s:7:"int(16)";s:3:"pid";s:12:"varchar(255)";s:5:"title";s:12:"varchar(255)";s:8:"onetitle";s:12:"varchar(255)";s:5:"image";s:12:"varchar(255)";s:4:"file";s:12:"varchar(255)";s:6:"switch";s:10:"tinyint(1)";}}
